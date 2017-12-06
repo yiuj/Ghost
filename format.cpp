d6fe050a403d59dd2d81d04f76ee03903abde158
Jonathan Yiu <yiuj@usc.edu>
2017-12-05 21:52:52 -0800

    words

--- format.cpp
+++ format.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     ifstream dict(argv[1]);
+    ofstream out(argv[2]);
 
     cout << "Working" << endl;
+
+    out << '{' << endl;
+    string word;
+    while(dict >> word) {
+        out << "\t\"" << word << "\"," << endl;
+    }
+    out << '}' << endl;
 }
